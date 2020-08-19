db9e48e118819942bdb71998b69edb75980b9ec8
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-08-19 14:33:41 +0900

    feat: import parts

--- README.md
+++ README.md
@@ -7,11 +7,16 @@
 - MediaQuery
 - ノーマライズ処理
 - Google Fonts(日本語：'Noto Sans JP',ローマ字: 'Kameron')
-- 
+- スクロール監視クラスの作成
+- ローダーの作成
 
+
+## 使用ライブラリ
+- swiper.js
+- intersection-observer.js(polyfill)
+- TweenMax.js
+- pace.js
+- 
 
 ## 参考
 [Code Mafia](https://twitter.com/codemafia0000)
-
-## 使用ライブラリ
-swiper.js
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //スライダー
   const hero = new HeroSlider('.swiper-container');
   hero.start();
+
+  //スクロール
+  const cb = function (el, isIntersecting) {
+    if(isIntersecting) {
+        const ta = new TweenTextAnimation(el);
+        ta.animate();
+    }
+  }
+  const so = new ScrollObserver('.tween-animate-title', cb);
 })
